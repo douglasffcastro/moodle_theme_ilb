1f2b3ec76da4739792307085cd5f683e726c4e7d
Douglas Castro <farias.douglascastro@gmail.com>
2021-12-20 13:52:49 -0300

    Add ilb theme settings

--- settings.php
+++ settings.php
@@ -20,77 +20,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 if ($ADMIN->fulltree) {
-    $settings = new theme_ilb_admin_settingspage_tabs('themesettingilb', get_string('configtitle', 'theme_ilb'));
-    $page = new admin_settingpage('theme_ilb_general', get_string('generalsettings', 'theme_ilb'));
-
-    // Preset.
-    $name = 'theme_ilb/preset';
-    $title = get_string('preset', 'theme_ilb');
-    $description = get_string('preset_desc', 'theme_ilb');
-    $default = 'default.scss';
-
-    $context = context_system::instance();
-    $fs = get_file_storage();
-    $files = $fs->get_area_files($context->id, 'theme_ilb', 'preset', 0, 'itemid, filepath, filename', false);
-
-    $choices = [];
-    foreach ($files as $file) {
-        $choices[$file->get_filename()] = $file->get_filename();
-    }
-    // These are the built in presets.
-    $choices['default.scss'] = 'default.scss';
-    $choices['plain.scss'] = 'plain.scss';
-
-    $setting = new admin_setting_configthemepreset($name, $title, $description, $default, $choices, 'ilb');
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $page->add($setting);
-
-    // Preset files setting.
-    $name = 'theme_ilb/presetfiles';
-    $title = get_string('presetfiles','theme_ilb');
-    $description = get_string('presetfiles_desc', 'theme_ilb');
-
-    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
-        array('maxfiles' => 20, 'accepted_types' => array('.scss')));
-    $page->add($setting);
-
-    // Background image setting.
-    $name = 'theme_ilb/backgroundimage';
-    $title = get_string('backgroundimage', 'theme_ilb');
-    $description = get_string('backgroundimage_desc', 'theme_ilb');
-    $setting = new admin_setting_configstoredfile($name, $title, $description, 'backgroundimage');
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $page->add($setting);
-
-    // Variable $body-color.
-    // We use an empty default value because the default colour should come from the preset.
-    $name = 'theme_ilb/brandcolor';
-    $title = get_string('brandcolor', 'theme_ilb');
-    $description = get_string('brandcolor_desc', 'theme_ilb');
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, '');
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $page->add($setting);
-
-    // Must add the page after definiting all the settings!
-    $settings->add($page);
-
-    // Advanced settings.
-    $page = new admin_settingpage('theme_ilb_advanced', get_string('advancedsettings', 'theme_ilb'));
-
-    // Raw SCSS to include before the content.
-    $setting = new admin_setting_scsscode('theme_ilb/scsspre',
-        get_string('rawscsspre', 'theme_ilb'), get_string('rawscsspre_desc', 'theme_ilb'), '', PARAM_RAW);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $page->add($setting);
-
-    // Raw SCSS to include after the content.
-    $setting = new admin_setting_scsscode('theme_ilb/scss', get_string('rawscss', 'theme_ilb'),
-        get_string('rawscss_desc', 'theme_ilb'), '', PARAM_RAW);
-    $setting->set_updatedcallback('theme_reset_all_caches');
-    $page->add($setting);
-
-    $settings->add($page);
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingilb', get_string('configtitle', 'theme_ilb'));
+    
+    require('settings/carrossel_settings.php');
+    require('settings/destaque_settings.php');
 }
