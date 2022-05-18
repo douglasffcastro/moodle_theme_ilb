3f9dbd1e6027a0e741befd33749abef0c15a33bd
Douglas Castro <farias.douglascastro@gmail.com>
2022-05-18 19:39:53 -0300

    Add new home page

--- config.php
+++ config.php
@@ -4,10 +4,21 @@ defined('MOODLE_INTERNAL') || die();
                                                                                            
 $THEME->name = 'ilb';                                                                                                                                                                                                                                                                                                                          
 $THEME->sheets = [];                                                                                                                                                                                                                                                                                                               
-$THEME->editor_sheets = [];                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+$THEME->editor_sheets = [];                                                                                                                                                                                                                                                                                                             
+$THEME->parents = ['boost'];                                                                                                                                                                                                                        
 $THEME->enable_dock = false;                                                                                                                                                                                                                           
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';                                                                                  
 $THEME->requiredblocks = '';   
 $THEME->usefallback = true;
 $THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
 
+
+$THEME->layouts = [
+  // The site home page.
+  'frontpage' => array(
+      'file' => 'frontpage.php',
+      'regions' => array('side-pre'),
+      'defaultregion' => 'side-pre',
+      'options' => array('nonavbar' => true)
+  ),
+];
